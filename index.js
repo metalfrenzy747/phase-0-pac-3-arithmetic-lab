391c5e5d4e7afb6d82fe5b714101f5c1dc7e9679
metalfrenzy747 <allenjeremy747@gmail.com>
2023-12-21 11:04:40 -0500

    commited

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+    return a + b;
+
+}
+function subtract(a, b) {
+    return a - b;
+
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+let n;
+function increment(n) {
+    return (n += 1);
+}
+increment(n);
+
+function decrement(n) {
+    return (n -= 1);
+}
+decrement(n);
+
+function makeInt(n) {
+    return (parseInt(n, 10));
+}
+makeInt(n)
+
+function preserveDecimal(n) {
+    return (parseFloat(n, 10));
+}
+preserveDecimal();
+
